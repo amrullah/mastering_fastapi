1daf0244d3ad2811a1b5a6d9cf7845078cf1a14b
amrullah <amrullah.zunzunia@gmail.com>
2022-12-30 12:41:07 +0530

    simple post endpoint

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,13 +1,23 @@
+from typing import Optional
+
 from fastapi import APIRouter
 from starlette import status
 from starlette.responses import Response
-
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/blog",
     tags=['blog']
 )
 
-@router.post("/new")
-def create_post():
-    pass
+
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+
+
+@router.post("/new/{id}")
+def create_post(blog: BlogModel, id: int, version: int = 1):
+    # blog is request body, id is path parameter and version is query parameter
+    return "OK"
